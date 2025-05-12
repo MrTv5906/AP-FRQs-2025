221b5097b179b8130450f1b3d1e54ff3792ef76d
MrTv5906 <Kelvinbaez5906@gmail.com>
2025-05-12 10:09:38 -0400

    Finalize partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -1,5 +1,16 @@
-//PART A
+//Part A
 public int walkDogs(int hour){
+    //establish amount of dogs available at DogWalkCompany
+    int numDogs = company.numAvaialableDogs();
 
-    
+    // if walker can walk more dogs than available
+    if(maxDogs > numDOgs){
+        company.updateDogs(hour, numDogs);
+        return numDogs;
+    } 
+    //if there are more dogs than the walker can handle
+    else {
+        company.updateDogs(hour, maxDogs);
+        return maxDogs;
+    }
 }
