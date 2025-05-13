d8475ad8ce19b4303bcb04ff04c0bcfdd4fb5bef
MrTv5906 <Kelvinbaez5906@gmail.com>
2025-05-13 16:21:02 -0400

    Update PartB.java

--- Q3/PartB.java
+++ Q3/PartB.java
@@ -1,1 +1,14 @@
+public ArrayList<Match> buildMatches(){
+  Arraylist<Match> matches = new Arraylist<Match>();
+  if(competitorList.size() % 2 == 0){
+    for(int i = 0; i < competitorList.size(); i++){
+      matches.add(new Match(competitorList.get(i)), competitorList.get(competitorList.size()-i-1));
+    }
+  } else {
+    for(int i = 1; i < competitorList.size(); i++){
+      matches.add(new Match(competitorList.get(i)), competitorList.get(competitorList.size()-i));
+  }
+}
+  return matches;
+}
 
