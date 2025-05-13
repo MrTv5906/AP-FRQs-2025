2126d30f169b97b96ad4185570c26476bcb9123e
MrTv5906 <Kelvinbaez5906@gmail.com>
2025-05-13 16:10:39 -0400

    Update PartA.java

--- Q3/PartA.java
+++ Q3/PartA.java
@@ -1,3 +1,8 @@
 public Round(String[] names){
-  
+  ArrayList<String> comeptitorList = new ArrayList<>();
+  int rank = 1
+  for(String name: names){
+    competitorList.add(new Competitor(name, rank);
+    rank++;
+  }
 }
