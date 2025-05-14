954af0b6082be6cecf8361863acb8fd4290fe7ff
MrTv5906 <Kelvinbaez5906@gmail.com>
2025-05-14 09:57:41 -0400

    Finalize PartB.java

--- Q4/PartB.java
+++ Q4/PartB.java
@@ -1,1 +1,16 @@
-
+public boolean clearPair(int row, int col){
+        int first = puzzle[row][col];
+        for(int i = row; i < puzzle.length; i++){
+            for(int j = 0; j < puzzle.length[0]; i++){
+                int second = puzzle[i][j];
+                if(i == row && j == col){
+                }
+                if((first == second) || (first + second == 10)){
+                    first == 0;
+                    second == 0;
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
