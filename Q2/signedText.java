923479916185412593c7bde8af7ce434d14509a6
MrTv5906 <Kelvinbaez5906@gmail.com>
2025-05-14 09:38:31 -0400

    Finalize signedText.java

--- Q2/signedText.java
+++ Q2/signedText.java
@@ -1,1 +1,25 @@
-
+public class signedText{
+    private String firstName;
+    private String lastName;
+    public signedText(String firstN, String lastN){
+        this.firstName = firstN;
+        this.lastName = lastN;
+    }
+    public String getSignature(){
+        if firstName.equals(""){
+            return lastName;
+        }
+        return firstName.substring(0,1) + "-" + lastName;
+    }
+    public String addSignature(parameter){
+        if(parameter.indexOf(this.getSignature()) == -1){
+            return parameter + getSignature();
+        }
+        else if(parameter.indexOf(getSignature()) == 0){
+            return paramter + getSignature()
+        }
+        else {
+            return parameter;
+        }
+    }
+}
