ce3cdd5d70154259dc150ce2f01e5178e6fe1f95
MrTv5906 <Kelvinbaez5906@gmail.com>
2025-05-14 09:47:53 -0400

    Update PartA.java

--- Q4/PartA.java
+++ Q4/PartA.java
@@ -1,8 +1,8 @@
 public SumOrSameGame(int numRows, int numCols){
-        int[][] gameBoard = new int[numRows][numCols];
-        for(int i = 0; i < gameBoard.length){
-            for(int j = 0; j < gameBoard[0].length){
-                gameBoard[i][j] = (int)(math.Random()*10+1);
+        int[][] puzzle = new int[numRows][numCols];
+        for(int i = 0; i < puzzle.length){
+            for(int j = 0; j < puzzle[0].length){
+                puzzle[i][j] = (int)(math.Random()*10+1);
             }
         }
     }
