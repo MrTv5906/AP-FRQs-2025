fd541e8928adc868d5e5025968502c1847f6a3fa
MrTv5906 <Kelvinbaez5906@gmail.com>
2025-05-22 09:39:35 -0400

    fix random num code PartA.java

--- Q4/PartA.java
+++ Q4/PartA.java
@@ -2,7 +2,7 @@ public SumOrSameGame(int numRows, int numCols){
         int[][] puzzle = new int[numRows][numCols];
         for(int i = 0; i < puzzle.length){
             for(int j = 0; j < puzzle[0].length){
-                puzzle[i][j] = (int)(math.Random()*10+1);
+                puzzle[i][j] = (int)(Math.random()*10+1);
             }
         }
     }
